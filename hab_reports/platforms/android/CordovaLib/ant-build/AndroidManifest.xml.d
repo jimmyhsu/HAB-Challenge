f1b9e86f3d66b7474f2a59c271eeb6289a7aa6ab
Jimmy Hsu <jimmyhsu@live.com>
2014-06-01 05:53:15 -0700

    fixed untracked files

--- hab_reports/platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-/Users/jhsu/Projects/HAB-Challenge/hab_reports/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/Users/jhsu/Projects/HAB-Challenge/hab_reports/platforms/android/CordovaLib/AndroidManifest.xml \
--- hab_reports/platforms/android/ant-build/HelloCordova-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-/Users/jhsu/Projects/hab-challenge/hab_reports/platforms/android/ant-build/HelloCordova-debug-unaligned.apk : \
-/Users/jhsu/Projects/hab-challenge/hab_reports/platforms/android/ant-build/HelloCordova.ap_ \
-/Users/jhsu/Projects/hab-challenge/hab_reports/platforms/android/ant-build/classes.dex \
-/Users/jhsu/Projects/HAB-Challenge/hab_reports/platforms/android/CordovaLib/ant-build/classes.jar \
