b7b92651bf5b0c29aef4d66715cf524e214b36bf
Tyler Brothers <tylerbrothers1@gmail.com>
2014-05-31 23:54:59 -0700

    testing camera functionality

--- hab_reports/platforms/ios/www/js/app.js
+++ hab_reports/platforms/ios/www/js/app.js
@@ -1,5 +1,32 @@
 angular.module('main', ['ionic']);
 
-function TwoCtrl($scope){
-	$scope.display = "show";
+function OverlordCtrl($scope){
+	$scope.isUserAccepted = false;
+
+	$scope.toggleUserAccepted = function(){
+		$scope.isUserAccepted = !$scope.isUserAccepted;
+	}
+}
+
+function DisclaimerCtrl(){
+}
+
+function CameraCtrl($scope){
+	$scope.picture = function Picture(){
+		alert("navigator: "+navigator.camera);
+		navigator.camera.getPicture(onSuccess, onFail, {
+	  				quality: 75,
+	  				destinationType: Camera.Destination.DATA_URL,
+	  				saveToPhotoAlbum: true
+	  			});
+		alert("something");
+	  	function onSuccess(imageData) {
+	  		alert("success");
+
+	  	}
+
+	  	function onFail(message){
+	  		alert("fail");
+	  	}
+	}
 }
--- hab_reports/www/js/app.js
+++ hab_reports/www/js/app.js
@@ -1,6 +1,32 @@
 angular.module('main', ['ionic']);
 
-function DisclaimerCtrl($scope){
+function OverlordCtrl($scope){
 	$scope.isUserAccepted = false;
+
+	$scope.toggleUserAccepted = function(){
+		$scope.isUserAccepted = !$scope.isUserAccepted;
+	}
 }
 
+function DisclaimerCtrl(){
+}
+
+function CameraCtrl($scope){
+	$scope.picture = function Picture(){
+		alert("navigator: "+navigator.camera);
+		navigator.camera.getPicture(onSuccess, onFail, {
+	  				quality: 75,
+	  				destinationType: Camera.Destination.DATA_URL,
+	  				saveToPhotoAlbum: true
+	  			});
+		alert("something");
+	  	function onSuccess(imageData) {
+	  		alert("success");
+
+	  	}
+
+	  	function onFail(message){
+	  		alert("fail");
+	  	}
+	}
+}
