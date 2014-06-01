2301d5596702ff8d69e281f8ca5a528a6804c1cf
Tyler Brothers <tylerbrothers1@gmail.com>
2014-06-01 00:37:45 -0700

    added overlord ctrl, moved header to overlordctrl

--- hab_reports/www/js/app.js
+++ hab_reports/www/js/app.js
@@ -13,13 +13,11 @@ function DisclaimerCtrl(){
 
 function CameraCtrl($scope){
 	$scope.picture = function Picture(){
-		alert("navigator: "+navigator.camera);
 		navigator.camera.getPicture(onSuccess, onFail, {
 	  				quality: 75,
 	  				destinationType: Camera.Destination.DATA_URL,
-	  				saveToPhotoAlbum: true
 	  			});
-		alert("something");
+
 	  	function onSuccess(imageData) {
 	  		alert("success");
 
